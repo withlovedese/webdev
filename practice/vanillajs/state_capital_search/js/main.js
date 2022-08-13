93a4ee610c5e4f8b64777df3e1283c7636194e52
Adesemi Elumaro <adesemi.elumaro@duke.edu>
2022-08-13 20:22:33 +0100

    customized it to nigerian states

--- practice/vanillajs/state_capital_search/js/main.js
+++ practice/vanillajs/state_capital_search/js/main.js
@@ -4,17 +4,23 @@ const matchList = document.getElementById('matchList');
 //search the states.json and filter
 
 const searchStates = async searchText => {
-    const res = await fetch('./data/states.json');
+    const res1 = await fetch('./data/states.json');
+    const res = await fetch('https://nigerian-states-info.herokuapp.com/api/v1/states');
     const states = await res.json();
+    const states1 = await res1.json();
 
-    //console.log(states);
+    //console.log(states.data[0].info);
+    //console.log(states1[0]);
+
+    const ngstates = states.data.map(state => state.info);
+    //console.log(ngstates[0]);
     //get matches to current text input
-    let matches = states.filter((state) => {
+    let matches = ngstates.filter((state) => {
         const regex = new RegExp(`^${searchText}`, 'gi'); //gi matches global and case insensitive
-        return state.name.match(regex) || state.abbr.match(regex);
+        return state.officialName.match(regex);
     });
 
-    //console.log(matches);
+    console.log(matches);
 
     if(searchText.length === 0){
         matches = [];
@@ -29,12 +35,16 @@ const outputHTML = matches => {
     if(matches.length > 0){
         const html = matches.map(match => `
             <div class="card card-body mb-4">
-                <h4>${match.name} (${match.abbr}) <span class="text-primary">${match.capital}</span></h4>
-                <small>lat: ${match.lat} / long: ${match.long}</small>
+                <h2>${match.officialName}</h2>
+                <h3 class="text-primary">Capital: ${match.Capital}</h3>
+                <p>Slogan: ${match.Slogan}</p>
+                <p>Website: <a href="https://${match.Website}" target="_blank">${match.Website}</a><p>
+                <small>Lat: ${match.Latitude} / Long: ${match.Longitude}</small>
             </div>
         `).join('');
         
         matchList.innerHTML = html;
+        //console.log(html);
     }
 }
 
