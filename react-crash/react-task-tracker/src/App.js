048a75c1dbe5aa2cc409715096b2ed9e617fe52d
Adesemi Elumaro <adesemi.elumaro@duke.edu>
2022-08-23 12:09:13 +0100

    more on components and props

--- react-crash/react-task-tracker/src/App.js
+++ react-crash/react-task-tracker/src/App.js
@@ -1,10 +1,46 @@
 import Header from "./components/Header"
+import Tasks from "./components/Tasks";
+import { useState } from 'react';
 
 function App() {
+
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Learn React',
+        day: 'tuesday',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: 'financial aid logistics',
+        day: 'monday',
+        reminder: false
+    },
+    {
+        id: 3,
+        text: 'Call Aima',
+        day: 'wednesday',
+        reminder: true
+    },
+    {
+        id: 4,
+        text: 'Film video',
+        day: 'friday',
+        reminder: false
+    },
+    
+  ])
+
+  //Delete Task
+  const deleteTask = (id) => {
+    console.log('delete', id)
+  }
 
   return (
     <div className="container">
       <Header/>
+      <Tasks tasks={tasks} onDelete={deleteTask}></Tasks>
     </div>
     
   );
